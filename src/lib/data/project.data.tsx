e27e15b91c4be398cef973b46c3814264926479d
TilokDutta <tilokdutta1234@gmail.com>
2025-06-29 00:16:07 +0530

    project added

--- src/lib/data/project.data.tsx
+++ src/lib/data/project.data.tsx
@@ -6,6 +6,12 @@ export type Projects = {
 
 export const projectsData: Projects[] = [
   {
+    title: 'Funroad-Multitenant Ecommerce website',
+    description:
+      "Developed a multi-tenant digital commerce platform using Next.js 15, Payload CMS, Tailwind CSS v4, and ShadcnUI, deployed on Vercel. It supports 100+ merchants with subdomain-based storefronts and role-based access. Integrated Stripe Connect for secure payments, automated vendor payouts, and a commission model, processing over 500 transactions monthly. The platform's CMS-backed APIs and SSR architecture improved onboarding speed by 50% and user retention by 30",
+    href:'https://multitenant-ecommerce-xi.vercel.app/'
+  },
+  {
     title: 'MOCKPrep-Interview Preparation App',
     description:
       'An AI-powered interview practice platform that offers real interview questions, personalized feedback, and tracks past and upcoming interviews to boost users interview readiness and confidence.',
